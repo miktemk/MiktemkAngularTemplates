ac716c06c2a9b71983c579e6f72928baa60bb396
Mikhail Temkine <miktemk@gmail.com>
2018-03-02 20:20:08 -0500

    more snippers + fix batch file

--- fetch-from-this-machine.bat
+++ fetch-from-this-machine.bat
@@ -1,6 +1,6 @@
 rem https://stackoverflow.com/questions/7170683/copy-all-files-and-folders-from-one-drive-to-another-drive-using-dos-command-pr
 
-XCOPY %APPDATA%\Code\User\snippets              .\vscode\snippets /i/y
-XCOPY %APPDATA%\Code\User\keybindings.json      .\vscode /y
-XCOPY %APPDATA%\Code\User\settings.json         .\vscode /y
-XCOPY %APPDATA%\Code\User\vsicons.settings.json .\vscode /y
+XCOPY /i/y %APPDATA%\Code\User\snippets              .\vscode\snippets
+XCOPY /y %APPDATA%\Code\User\keybindings.json      .\vscode\
+XCOPY /y %APPDATA%\Code\User\settings.json         .\vscode\
+XCOPY /y %APPDATA%\Code\User\vsicons.settings.json .\vscode\
