18d38e1bc28d5e4e81187d4401298373308b29e5
Mikhail Temkine <miktemk@gmail.com>
2018-03-15 10:47:33 -0400

    beautify + more TS snippets

--- fetch-from-this-machine.bat
+++ fetch-from-this-machine.bat
@@ -1,5 +1,7 @@
 rem https://stackoverflow.com/questions/7170683/copy-all-files-and-folders-from-one-drive-to-another-drive-using-dos-command-pr
 
+cd "C:\_mik\gh\MyProjectTemplates"
+
 XCOPY /i/y %APPDATA%\Code\User\snippets              .\vscode\snippets
 XCOPY /y %APPDATA%\Code\User\keybindings.json      .\vscode\
 XCOPY /y %APPDATA%\Code\User\settings.json         .\vscode\
