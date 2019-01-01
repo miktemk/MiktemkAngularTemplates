80e247030917c0f418d81781cd710440b0ee2e43
Mikhail Temkine <miktemk@gmail.com>
2018-12-31 20:46:56 -0500

    grma + reg hack + ChewMyText + snippets

--- sublime/ChewMyText.py
+++ sublime/ChewMyText.py
@@ -19,6 +19,34 @@ class ChewMyTextCommand(sublime_plugin.TextCommand):
                 text = self.view.substr(region)
                 text = self.ChewText(text)
                 self.view.replace(edit, region, text)
+    def RoundAllDecimalNumbers(self, text):
+        # LINK: https://regex101.com/r/NI2IY3/3
+        listy = []
+        matches = re.finditer(re.compile(r'\d+\.\d\d\d+'), text)
+        for m in matches:
+            numStr = m.group(0)
+            replacement = "{0:0.0f}".format(float(numStr))
+            listy += [(numStr, replacement, m.start(0))]
+        listy = sorted(listy, key=itemgetter(2), reverse=True)
+        for (numStr, replacement, start) in listy:
+            text = text[:start] + replacement + text[start + len(numStr):]
+        return text
+
+    def MultiplyAllNumbersInRegexGroups(self, text, regex, factor):
+        listy = []
+        matches = re.finditer(re.compile(regex), text)
+        for m in matches:
+            for iGroup in range(len(m.groups())):
+                numStr = m.group(iGroup+1)
+                num = float(numStr)
+                num *= factor
+                replacement = "{0:0.0f}".format(num)
+                listy += [(numStr, replacement, m.start(iGroup+1))]
+        listy = sorted(listy, key=itemgetter(2), reverse=True)
+        for (numStr, replacement, start) in listy:
+            text = text[:start] + replacement + text[start + len(numStr):]
+        return text
+
     def ChewText(self, text):
         #.... Format PATH from Commnad Prompt
         # text = text.replace("\n", "")
@@ -34,15 +62,16 @@ class ChewMyTextCommand(sublime_plugin.TextCommand):
         # self.view.replace(edit, region, text)
 
         #.... SVG: Reduce the file size of pngtosvg.com SVG by rounding off the decimal points
-        # LINK: https://regex101.com/r/NI2IY3/3
-        listy = []
-        matches = re.finditer(re.compile(r'\d+\.\d\d\d+'), text)
-        for m in matches:
-            numStr = m.group(0)
-            replacement = "{0:0.0f}".format(float(numStr))
-            listy += [(numStr, replacement, m.start(0))]
-        listy = sorted(listy, key=itemgetter(2), reverse=True)
-        for (numStr, replacement, start) in listy:
-            text = text[:start] + replacement + text[start + len(numStr):]
+        # text = self.RoundAllDecimalNumbers(text)
+
+        #.... SVG: LeshyLabs - apply coordinate reduction
+        widthBefore = 2413
+        widthAfter = 800
+        reductionFactor = float(widthAfter) / widthBefore
+        # LINK: https://regex101.com/r/Yj99Mo/1
+        text = self.MultiplyAllNumbersInRegexGroups(text, r'"x":(\d+),"y":(\d+),"w":(\d+),"h":(\d+)', reductionFactor)
+        # LINK: https://regex101.com/r/Yj99Mo/3
+        text = self.MultiplyAllNumbersInRegexGroups(text, r'{"w":(\d+),"h":(\d+)}', reductionFactor)
+
 
         return text
